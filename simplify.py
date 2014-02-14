9f87a3e74c47d65922dcf9d5213eb805339bfbbb
milkbread <milkbread@freenet.de>
2014-02-14 19:24:52 +0100

    Implement output of rectangles and buffers

--- simplify.py
+++ simplify.py
@@ -4,7 +4,7 @@ import os
 import sys
 import json
 from osgeo import ogr, osr
-from shapely.geometry import Point, LineString
+from shapely.geometry import Point, LineString, box
 
 from pyrtree import RTree, Rect
 
@@ -75,17 +75,24 @@ def saveToGeoJSON(filename, geometries):
 		  },
 		  "properties": {}
 		}
-		# cleaning for GeoJSON...by replacing '(...)' with '[...]'
-		geojson_points = []
-		for coord in list(geom.coords):
-			geojson_points.append([coord[0],coord[1]])
 		# add geometry to geoJSON-geometry-dummy
 		geojson = feauture_dummy.copy()
 		geojson['geometry']['type'] = geom.geometryType()
+		# cleaning for GeoJSON...by replacing '(...)' with '[...]'
+		geojson_points = []
+		if geom.type == 'Point' or geom.type == 'LineString':
+			coords = list(geom.coords)
+		elif geom.type == 'Polygon':
+			coords = list(geom.exterior.coords)
+		for coord in coords:
+			geojson_points.append([coord[0],coord[1]])
+		# make GeoJSON
 		if geom.geometryType() == 'Point':
 			geojson['geometry']['coordinates'] = geojson_points[0]
 		elif geom.geometryType() == 'LineString':
 			geojson['geometry']['coordinates'] = geojson_points
+		elif geom.geometryType() == 'Polygon':
+			geojson['geometry']['coordinates'] = [geojson_points]
 		features.append(geojson)
 	# add features to feature-collection-dummy
 	featurecoll_geojson = featcoll_dummy.copy()
@@ -115,35 +122,46 @@ def main(argv=None):
    	rect_res = t.query_rect( Rect(x,y,xx,yy) )'''
 
    	t = RTree()
+   	rectangles = []
    	for line_ in lines:
    		#print dir(line_)
    		#print line_.bounds
+   		b = box(line_.bounds[0],line_.bounds[1],line_.bounds[2],line_.bounds[3])
+   		rectangles.append(b)
+   		#print list(b.exterior.coords)
    		t.insert(line_,Rect(line_.bounds[0],line_.bounds[1],line_.bounds[2],line_.bounds[3]))
 
    	intersection_points = []
+   	buffered_points = []
    	for point_ in points:
    		#print dir(point_)
    		#print point_.x, point_.y
    		#point_res = t.query_point( (point_.x,point_.y) )
    		#print point_res
    		real_point_res = [r.leaf_obj() for r in t.query_point( (point_.x,point_.y) ) if r.is_leaf()]
-   		#print real_point_res[0]
+   		#Point(ogr_geometry.GetX(),ogr_geometry.GetY())
    		distance = real_point_res[0].project(point_)
-   		buffered_point = point_.buffer(distance+1)
-   		if line_.intersects(buffered_point):
+   		#print distance
+   		buffered_point = point_.buffer(0.09)
+   		print real_point_res[0].intersects(buffered_point)
+   		buffered_points.append(buffered_point)
+   		'''if line_.intersects(buffered_point):
    			intersection = line_.intersection(buffered_point)
    			#print dir(intersection)
    			intersection_point = intersection.interpolate(distance+1.5)
    			intersection_points.append(intersection_point)
+'''
 
+	intersection_points = findCPointBySimpleBuffer(lines, points, 0.05)
 
-	#intersection_points = findCPointBySimpleBuffer(lines, points, 0.05)
+	#saveToGeoJSON('points.json',points)
+	#saveToGeoJSON('lines.json',lines)
+	#saveToGeoJSON('intersection_points.json',intersection_points)
 
-	saveToGeoJSON('points.json',points)
-	saveToGeoJSON('lines.json',lines)
-	saveToGeoJSON('intersection_points.json',intersection_points)
+	saveToGeoJSON('rectangles.json',rectangles)
+	saveToGeoJSON('buffered_points.json',buffered_points)
 
-	os.system("topojson -o all_features_topo.json points.json lines.json intersection_points.json");
+	os.system("topojson -o all_features_topo.json points.json lines.json intersection_points.json rectangles.json buffered_points.json");
 
 if __name__ == "__main__":
     sys.exit(main())
