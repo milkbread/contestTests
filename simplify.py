7f6a704c4b7ac12eaef43e98acafa7b7c20fb6f9
milkbread <milkbread@freenet.de>
2014-03-02 17:53:39 +0100

    Implement intersection with RTree

--- simplify.py
+++ simplify.py
@@ -8,21 +8,30 @@ from shapely.geometry import Point, LineString, box
 
 from pyrtree import RTree, Rect
 
-def findCPointByRTree(points, r_tree):
+def findCPointByRTree(points, r_tree, buffer_distance=0):
 
-	intersection_points = []
+	intersections = []
    	buffered_points = []
    	for point_ in points:
-   		real_point_res = [r.leaf_obj() for r in r_tree.query_point( (point_.x,point_.y) ) if r.is_leaf()]
-   		distance = real_point_res[0].project(point_)
-   		buffered_point = point_.buffer(0.09)
-   		buffered_points.append(buffered_point)
-   		# if line_.intersects(buffered_point):
-   		# 	intersection = line_.intersection(buffered_point)
-   		# 	intersection_point = intersection.interpolate(distance+1.5)
-   		# 	intersection_points.append(intersection_point)
 
-   	return intersection_points, buffered_points
+   		resulting_lines = [r.leaf_obj() for r in r_tree.query_point( (point_.x,point_.y) ) if r.is_leaf()]
+   		# print 'Number of resulting lines:', len(resulting_lines)
+
+   		for line in resulting_lines:
+	   		# distance = line.project(point_)
+	   		distance = line.distance(point_)
+	   		# prevent too big buffer distances ('failure' in RTree)
+	   		if buffer_distance < distance:
+	   			distance = buffer_distance
+	   		buffered_point = point_.buffer(distance*1.1)
+	   		buffered_points.append(buffered_point)
+   			intersects = line.intersects(buffered_point)
+   			# print intersects
+   			if line.intersects(buffered_point):
+   				intersection = line.intersection(buffered_point)
+   				intersections.append(intersection)
+
+   	return intersections, buffered_points
 
 def findCPointBySimpleBuffer(lines, points, buffer_distance=0):
 	intersection_points = []
@@ -110,16 +119,28 @@ def saveToGeoJSON(filename, geometries, indizes=None):
 		geojson['geometry']['type'] = geom.geometryType()
 		# cleaning for GeoJSON...by replacing '(...)' with '[...]'
 		geojson_points = []
+		# print geom.type
 		if geom.type == 'Point' or geom.type == 'LineString':
 			coords = list(geom.coords)
+			for coord in coords:
+				geojson_points.append([coord[0],coord[1]])
 		elif geom.type == 'Polygon':
 			coords = list(geom.exterior.coords)
-		for coord in coords:
-			geojson_points.append([coord[0],coord[1]])
+			for coord in coords:
+				geojson_points.append([coord[0],coord[1]])
+		elif geom.type == 'MultiLineString':
+			# print geom.type
+			for geom_ in geom.geoms:
+				coords = geom_.coords
+				geojson_points_ = []
+				for coord in coords:
+					geojson_points_.append([coord[0],coord[1]])
+				geojson_points.append(geojson_points_)
+			# print geojson_points
 		# make GeoJSON
 		if geom.geometryType() == 'Point':
 			geojson['geometry']['coordinates'] = geojson_points[0]
-		elif geom.geometryType() == 'LineString':
+		elif geom.geometryType() == 'LineString' or geom.geometryType() == 'MultiLineString':
 			geojson['geometry']['coordinates'] = geojson_points
 		elif geom.geometryType() == 'Polygon':
 			geojson['geometry']['coordinates'] = [geojson_points]
@@ -146,6 +167,10 @@ def main(argv=None):
 		transform_ = argv[1]
 	except:
 		transform_ = None
+	try:
+		buffer_ = int(argv[2])
+	except:
+		buffer_ = 0.05
 
 	lines_input = open('training_data/lines_out.txt', 'r')
 	points_input = open('training_data/points_out.txt', 'r')
@@ -153,24 +178,24 @@ def main(argv=None):
 	lines, lines_indizes, lines_r_tree = readInput(lines_input, transform_)
 	points, points_indizes = readInput(points_input, transform_)
 
-	intersection_points_r_tree, buffered_points = findCPointByRTree(points, lines_r_tree)
+	intersections_r_tree, buffered_points = findCPointByRTree(points, lines_r_tree, buffer_)
 
-	intersection_points = findCPointBySimpleBuffer(lines, points, 0.05)
+	intersection_points = findCPointBySimpleBuffer(lines, points, buffer_)
 
 	saveToGeoJSON('results/points.json',points, points_indizes)
 	saveToGeoJSON('results/lines.json',lines, lines_indizes)
 	saveToGeoJSON('results/intersection_points.json',intersection_points)
-	saveToGeoJSON('results/intersection_points_r_tree.json',intersection_points_r_tree)
+	saveToGeoJSON('results/intersections_r_tree.json',intersections_r_tree)
 
 	saveToGeoJSON('results/rectangles.json',exportRectangles(lines), lines_indizes)
-	saveToGeoJSON('results/buffered_points.json',buffered_points, points_indizes)
+	saveToGeoJSON('results/buffered_points.json',buffered_points)
 
 	os.system("topojson --id-property id \
 		 -o results/all_features_topo.json \
 		 results/points.json \
 		 results/lines.json \
 		 results/intersection_points.json \
-		 results/intersection_points_r_tree.json \
+		 results/intersections_r_tree.json \
 		 results/rectangles.json \
 		 results/buffered_points.json");
 
